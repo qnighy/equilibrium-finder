4e1044aa68421d7cd55d02b06775fe381e6d77e7
Masaki Hara <ackie.h.gmai@gmail.com>
2022-01-23 15:58:41 +0900

    Start implementing UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { numPlayers, playerNames } = state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nash equilibrium finder</h1>
+      {
+        playerNames.map((playerName, playerId) => (
+          <div key={playerId}>
+            <h2>Player <input value={playerName} onChange={(e) => dispatch({ type: "setPlayerName", playerId, playerName: e.currentTarget.value })} /></h2>
+          </div>
+        ))
+      }
+      <button onClick={() => dispatch({ type: "addPlayer" })}>➕ Add a player</button>
+      <button onClick={() => dispatch({ type: "removePlayer" })}>➖ Remove the last player</button>
     </div>
   );
+}
+
+type State = {
+  numPlayers: number;
+  playerNames: string[];
+  allPlayerStrategies: string[][];
+};
+
+const initialState: State = {
+  numPlayers: 2,
+  playerNames: ["1", "2"],
+  allPlayerStrategies: [["rock", "paper", "scissors"], ["rock", "paper", "scissors"]],
+};
+
+type Action =
+  | { type: "addPlayer" }
+  | { type: "removePlayer" }
+  | { type: "setPlayerName", playerId: number, playerName: string };
+
+function reducer(prevState: State, action: Action): State {
+  switch (action.type) {
+    case "addPlayer":
+      return {
+        ...prevState,
+        numPlayers: prevState.numPlayers + 1,
+        playerNames: [...prevState.playerNames, `${prevState.numPlayers + 1}`],
+        allPlayerStrategies: [...prevState.allPlayerStrategies, ["rock", "paper", "scissors"]],
+      };
+    case "removePlayer": {
+      const numPlayers = prevState.numPlayers - 1;
+      if (numPlayers <= 1) return prevState;
+      return {
+        ...prevState,
+        numPlayers,
+        playerNames: prevState.playerNames.slice(0, numPlayers),
+        allPlayerStrategies: prevState.allPlayerStrategies.slice(0, numPlayers),
+      };
+    }
+    case "setPlayerName": {
+      const playerNames = [...prevState.playerNames];
+      playerNames[action.playerId] = action.playerName;
+      return {
+        ...prevState,
+        playerNames,
+      };
+    }
+  }
+  return prevState;
 }
 
 export default App;
